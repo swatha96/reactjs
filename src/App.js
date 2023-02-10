89ae33d0b64a5e650f5e651aa9b4400d1a57416b
swethiru15 <swethiru15@gmail.com>
2023-02-10 12:04:23 +0530

    sample

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          My Application
+        </p>
+        
+
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+    </div>
+    <div cass>
+      <body>
+
+      </body>
+    </div>
+    </>
+  );
+}
+
+export default App;
